4edefcd4c68004adfdb3e073e5d6ece1c20c07f0
이상혁 <32203155@dankook.ac.kr>
2026-06-27 17:34:04 +0900

    chore: 폴더 구조 설정 (#4)

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,8 +1,8 @@
 import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "./assets/vite.svg";
-import heroImg from "./assets/hero.png";
-import "./App.css";
+import reactLogo from "../assets/icons/react.svg";
+import viteLogo from "../assets/icons/vite.svg";
+import heroImg from "../assets/images/hero.png";
+import "../styles/App.css";
 
 function App() {
   const [count, setCount] = useState(0);
@@ -32,7 +32,7 @@ function App() {
         <div>
           <h1>Get started</h1>
           <p>
-            Edit <code>src/App.tsx</code> and save to test <code>HMR</code>
+            Edit <code>src/app/App.tsx</code> and save to test <code>HMR</code>
           </p>
         </div>
         <button
